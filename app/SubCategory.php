9a9d162463f4e18fb218a2f4b48239a69ba3ed99
fearless-hasan <hasan.bubt.40@gmail.com>
2018-02-08 19:32:31 +0600

    recheck Model

--- app/SubCategory.php
+++ app/SubCategory.php
@@ -9,6 +9,6 @@ class SubCategory extends Model
     use SoftDeletes;
 
     protected $fillable = [
-        'sub_category_name', 'sub_category_photo','sub_category_detail', 'publication_status'
+        'category_id', 'sub_category_name', 'sub_category_photo','sub_category_detail', 'publication_status'
     ];
 }
