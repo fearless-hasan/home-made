4d8c263300d270113a82e623bf1541ebce69d2d4
Md Hasan Ali <hasan.bubt.40@gmail.com>
2018-02-10 23:39:00 +0600

    checked at 11.38pm 10.2.18

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    public function getAllReview(){
+
+    }
+    public function getReviewById($id){
+
+    }
+
+    public function addReview(Request $request){
+
+    }
+    public function deleteReview($id){
+
+    }
+    public function publishReview($id){
+
+    }
+    public function unpublishReview($id){
+
+    }
+    public function updateReview(Request $request,$id){
+
+    }
 }
